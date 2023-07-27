c85bc6a82b271251e86228616ed630950e18cf77
ShenYueYue-Sara <shenyueyue22@mails.ucas.edu.cn>
2023-07-28 00:15:23 +0800

    IMPORTANT ! add self.med_nobs

--- code/point_source/gaia_magerror.py
+++ code/point_source/gaia_magerror.py
@@ -60,7 +60,6 @@ class Edr3LogMagUncertainty(object):
                 raise ValueError(f'Uncertainties can be estimated on in the range {band}[4, 21]')
             elif mag_range[0] > mag_range[1]:
                 raise ValueError('Malformed magnitude range')
-        #
         xx = np.linspace(mag_range[0], mag_range[1], mag_samples)
         __cols = self.__compute_nobs(band, xx, nobs)
         __dc = {f'mag_{band}': xx, **__cols}
@@ -92,44 +91,51 @@ class MagError(Edr3LogMagUncertainty):
     method2: from observation mag_magerr relation (corrected Nobs effect)
     '''
     
-    def __init__(self, spline_csv=spline_csv, bands=['Gmag','G_BPmag','G_RPmag']):
+    def __init__(self, sample_obs=None, med_nobs=None, spline_csv=spline_csv, bands=['Gmag','G_BPmag','G_RPmag']):
         super(MagError,self).__init__(spline_csv)
         self.bands = bands
         self.spline_g = self._Edr3LogMagUncertainty__splines['g']
         self.spline_bp = self._Edr3LogMagUncertainty__splines['bp']
         self.spline_rp = self._Edr3LogMagUncertainty__splines['rp']
-    
+        if med_nobs:
+            self.med_nobs = med_nobs
+        else:
+            if sample_obs is not None:
+                self.med_nobs = self.extract_med_nobs(sample_obs)
+            else:
+                print('please enter med_nobs OR sample_obs')
+        
     def extract_med_nobs(self, sample_obs, nobs=['phot_g_n_obs','phot_bp_n_obs','phot_rp_n_obs']):
         # extract the median value of n_obs(number of observation)
-        med_g_nobs = int(np.median(sample_obs[nobs[0]]))
-        med_bp_nobs = int(np.median(sample_obs[nobs[1]]))
-        med_rp_nobs = int(np.median(sample_obs[nobs[2]]))
-        return med_g_nobs, med_bp_nobs, med_rp_nobs
+        med_nobs = []
+        for i in range(3):
+            med = int(np.median(sample_obs[nobs[i]]))
+            med_nobs.append(med)
+        return med_nobs
     
-    def random_n_obs(self, n_stars, med_g_nobs, med_bp_nobs, med_rp_nobs):
+    def random_n_obs(self, n_stars):
         # generate n_obs(number of observation) which obeys poisson(miu=med_nobs)
-        g_n_obs = np.random.poisson(med_g_nobs, n_stars)
-        bp_n_obs = np.random.poisson(med_bp_nobs, n_stars)
-        rp_n_obs = np.random.poisson(med_rp_nobs, n_stars)
+        g_n_obs = np.random.poisson(self.med_nobs[0], n_stars)
+        bp_n_obs = np.random.poisson(self.med_nobs[1], n_stars)
+        rp_n_obs = np.random.poisson(self.med_nobs[2], n_stars)
         return g_n_obs, bp_n_obs, rp_n_obs
     
-    def estimate_med_photoerr(self, sample_syn, sample_obs):
+    def estimate_med_photoerr(self, sample_syn):
         # return statistic value (MEDIAN) of the error distribution, considering number of observation
         # step 1 : generate synthetic n_obs for each band
         n_stars = len(sample_syn)
-        med_g_nobs, med_bp_nobs, med_rp_nobs = self.extract_med_nobs(sample_obs)
-        g_n_obs, bp_n_obs, rp_n_obs = self.random_n_obs(n_stars, med_g_nobs, med_bp_nobs, med_rp_nobs)
+        g_n_obs, bp_n_obs, rp_n_obs = self.random_n_obs(n_stars)
         # step 2 : calculate mag_err when Nobs = 200(for G) / 20(for BP,RP)
         g_med_err = 10**(self.spline_g(sample_syn[self.bands[0]]) - np.log(np.sqrt(g_n_obs) / np.sqrt(200)))
         bp_med_err = 10**(self.spline_bp(sample_syn[self.bands[1]]) - np.log(np.sqrt(bp_n_obs) / np.sqrt(20)))
         rp_med_err = 10**(self.spline_rp(sample_syn[self.bands[2]]) - np.log(np.sqrt(rp_n_obs) / np.sqrt(20)))
         return g_med_err, bp_med_err, rp_med_err
     
-    def syn_sample_photoerr(self, sample_syn, sample_obs):
+    def syn_sample_photoerr(self, sample_syn):
         # return synthetic band mag (with statistic error) which obey with N(band,band_med_err)
         n_stars = len(sample_syn)
         normal_sample = np.random.normal(n_stars)
-        g_med_err, bp_med_err, rp_med_err = self.estimate_med_photoerr(sample_syn, sample_obs)
+        g_med_err, bp_med_err, rp_med_err = self.estimate_med_photoerr(sample_syn)
         g_syn = (g_med_err/0.67) * normal_sample + sample_syn[self.bands[0]]
         bp_syn = (bp_med_err/0.67) * normal_sample + sample_syn[self.bands[1]]
         rp_syn = (rp_med_err/0.67) * normal_sample + sample_syn[self.bands[2]]
@@ -141,12 +147,14 @@ def main():
     sample = pd.read_csv("/home/shenyueyue/Projects/Cluster/data/Cantat-Gaudin_2020/%s.csv"%name, usecols=usecols)
     sample = sample.dropna().reset_index(drop=True)
 
-    e = MagError()
-    g_med_err, bp_med_err, rp_med_err = e.estimate_med_photoerr(sample,sample)
-    g_syn, bp_syn, rp_syn = e.syn_sample_photoerr(sample,sample)
-    sample['Gmag_err_syn'], sample['G_BPmag_err_syn'], sample['G_RPmag_err_syn'] = g_med_err, bp_med_err, rp_med_err
-    sample['Gmag_syn'], sample['G_BPmag_syn'], sample['G_RPmag_syn'] = g_syn, bp_syn, rp_syn
-    sample.to_csv("/home/shenyueyue/Projects/Cluster/data/%s_syn.csv"%name,index=False)
+    e = MagError(sample_obs=sample)
+    g_med_err, bp_med_err, rp_med_err = e.estimate_med_photoerr(sample_syn=sample)
+    # g_syn, bp_syn, rp_syn = e.syn_sample_photoerr(sample,sample)
+    # sample['Gmag_err_syn'], sample['G_BPmag_err_syn'], sample['G_RPmag_err_syn'] = g_med_err, bp_med_err, rp_med_err
+    # sample['Gmag_syn'], sample['G_BPmag_syn'], sample['G_RPmag_syn'] = g_syn, bp_syn, rp_syn
+    # sample.to_csv("/home/shenyueyue/Projects/Cluster/data/%s_syn.csv"%name,index=False)
+    
+    
 
 if __name__=="__main__":
     main()
