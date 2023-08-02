0f46eb27e5bf20e2f2f5fb2ca57b249badd22f09
ShenYueYue-Sara <shenyueyue22@mails.ucas.edu.cn>
2023-08-02 21:33:31 +0800

    change lnlike() to theta_part & add mcmc parallel

--- code/point_source/mcmc.py
+++ code/point_source/mcmc.py
@@ -7,7 +7,7 @@ import logging
 import traceback
 import numpy as np
 import pandas as pd
-import multiprocessing as mp
+from multiprocessing import Pool
 from joblib import Parallel,delayed
 
 from mock_cmd import MockCMD
@@ -45,9 +45,12 @@ def download_isochrone(isochrones_dir, logage_grid, mh_grid, dm, n_jobs=10):
     #     )
     #     pbar.update(1) 
 
-def lnlike(theta, n_stars, step, sample_obs): # logage, mh, fb, dm = theta
+def lnlike(theta_part, n_stars, step, sample_obs): # logage, mh, fb, dm = theta
     start_time = time.time()
-    logage, mh, fb, dm = theta
+    #logage, mh, fb, dm = theta
+    fb, dm = 0.35, 5.55
+    logage, mh = theta_part
+    theta = logage, mh, fb, dm
     try:
         if (logage>10.0) or (logage<6.6) or (mh<-0.9) or (mh>0.7) or (fb<0.05) or (fb>1) or (dm<2) or (dm>20):
             return -1e50
@@ -87,21 +90,65 @@ def test_randomness(sample_obs, theta, n_stars, time=1000, step=(0.05, 0.1)): # 
     fig.show()
 
 def main():
-    '''
+
     # download isochrone
-    logage_grid = (6.6, 10, 0.05)
-    mh_grid = (-0.9, 0.7, 0.1)
+    logage_grid = (6.6, 10, 0.01)
+    mh_grid = (-0.9, 0.7, 0.01)
     dm = 5.55 
     download_isochrone(isochrones_dir=isochrones_dir, logage_grid=logage_grid, mh_grid=mh_grid, dm=dm)
-    ''' 
-     
+
+    '''
     # read smaple_obs
     name = 'Melotte_22'
     usecols = ['Gmag','G_BPmag','G_RPmag','phot_g_n_obs','phot_bp_n_obs','phot_rp_n_obs']
     sample_obs = pd.read_csv("/home/shenyueyue/Projects/Cluster/data/Cantat-Gaudin_2020/%s.csv"%(name), usecols=usecols)
     sample_obs = sample_obs.dropna().reset_index(drop=True)
+
+    # MCMC
+    n_stars = 942
+    step = (0.05, 0.1)
+    # parameter
+    theta_part = np.array([7.89, 0.032]) # after round theta = (8.00, 0.0) 
+    scale = np.array([0.05, 0.1])
+    ndim = 2
     
+    # set up the MCMC sampler
+    nwalkers = 50
+    # define the step sizes for each parameter
+    # moves = [emcee.moves.StretchMove(a=step_sizes[i]) for i in range(ndim)]
+    # create an array of initial positions with small random perturbations
+    p0 = np.round((theta_part + scale * np.random.randn(nwalkers, ndim)), decimals=2)
     
+    # parallelization
+    with Pool() as pool:   
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, lnlike, args=(n_stars, step, sample_obs), pool=pool) # , moves=moves
+        # burn-in
+        nburn = 1000
+        start_burn = time.time()
+        pos,_,_ = sampler.run_mcmc(p0, nburn, progress=True)
+        end_burn = time.time()
+        time_burn = end_burn - start_burn
+        print(f"burn-in time : {time_burn:.1f} seconds")
+        sampler.reset()
+        
+        # run the MCMC sampler
+        nsteps = 2000
+        start_run = time.time()
+        sampler.run_mcmc(pos, nsteps, progress=True)
+        end_run = time.time()
+        time_run = end_run - start_run
+        print(f"run time : {time_run:.1f} seconds")
+    
+    samples = sampler.chain[:, :, :].reshape((-1, ndim))
+    fig = corner.corner(samples,
+        labels=[r'log(age)',r'[M/H]',r'$f_b$',r'DM'],
+            quantiles=[0.16, 0.5, 0.84],
+            show_titles=True, 
+            title_kwargs={"fontsize": 12},
+            title_fmt = '.4f')
+    fig.savefig(f"/home/shenyueyue/Projects/Cluster/code/point_source/figure/mcmc_w{nwalkers}_b{nburn}_r{nsteps}.png",bbox_inches='tight')
+    '''
+    '''
     # test the randomness of lnlikelihood() vs logage
     theta1 = (7.89, 0.032, 0.35, 5.55)
     theta2 = (8.89, 0.032, 0.35, 5.55)
@@ -113,43 +160,6 @@ def main():
     for n_stars in n:
         print(f"test_randomness for n_stars={n_stars}")
         test_randomness(sample_obs, theta1, n_stars)
-    
-    
     '''
-    # MCMC
-    logage, mh, fb, dm = theta
-    theta = (7.89, 0.032, 0.35, 5.55)
-    n_starts = 942
-    step = (0.05, 0.1)
-    
-    # set up the MCMC sampler
-    nwalkers = 10
-    ndim = 4
-    # define the step sizes for each parameter
-    step_sizes = np.array([0.05, 0.2, 0.1, 2])
-    moves = [emcee.moves.StretchMove(a=step_sizes[i]) for i in range(ndim)]
-    # create an array of initial positions with small random perturbations
-    p0 = np.round((np.array([7.80, 0.02, 0.35, 5.55]) + step_sizes * np.random.randn(nwalkers, ndim)), decimals=2)
-    
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnlike, args=(n_stars, sample_obs), moves=moves)
-    # burn-in
-    nburn = 50
-    pos,_,_ = sampler.run_mcmc(p0, nburn) #, progress=True
-    sampler.reset()
-    # run the MCMC sampler
-    nsteps = 200
-    sampler.run_mcmc(pos, nsteps, progress=True)
-    
-    samples = sampler.chain[:, :, :].reshape((-1, ndim))
-    fig = corner.corner(samples,
-        labels=[r'log(age)',r'[M/H]',r'$f_b$',r'DM'],
-            quantiles=[0.16, 0.5, 0.84],
-            show_titles=True, 
-            title_kwargs={"fontsize": 12},
-            title_fmt = '.4f')
-    fig.savefig('/home/shenyueyue/Projects/Cluster/code/point_source/figure/emcee_result.png',bbox_inches='tight')
-    '''
-    
 if __name__ == '__main__':
     main()
-
